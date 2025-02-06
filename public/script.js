c990b5a120f352ecb3f3e9bd41d2f7ddd8a650ff
vsheo <vireshsheoratan@gmail.com>
2025-02-06 18:07:57 +0100

    js refactor #4

--- public/script.js
+++ public/script.js
@@ -1,67 +1,25 @@
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
 
-const sectionKaartje = document.querySelector('.kaartje');
-
-
-const buttonVisitekaartje = document.querySelector('.visitekaartje');
-const buttonMediahuis = document.querySelector('.ul-buttons li:nth-of-type(2)');
-const buttonAccessdash = document.querySelector('.ul-buttons li:nth-of-type(3)');
-const buttonILW = document.querySelector('.ul-buttons li:nth-of-type(4)');
-console.log(buttonVisitekaartje);
-console.log(buttonMediahuis);
-console.log(buttonAccessdash);
-console.log(buttonILW);
-
-
-const ulButtons = document.querySelector('.ul-buttons');
-
-
-whichButton(ulButtons);
-
-
-function whichButton(clickedButton) {
-	clickedButton.addEventListener('click', (e) => {
-		// zoek de button die geklikt wordt
-		console.log(e.target);
-
-		// selecteer de meest dichtstbijzijnde li bij de geklikte button
-		let li = e.target.closest("li");
-		// zoek de parrent van die li
-		let ul = li.parentElement;
-		// zoek de index van de li. is het de 1ste, 2de, 3de of 4de in de ul.
-		let index = Array.from(ul.children).indexOf(li);
-		console.log("index: " + index + " in de ul");
-
-		// voeg classes toe afhankelijk van de geklikte button
-		if (index == 0) {
-			console.log("eerste button");
-			buttonVisitekaartje.classList.toggle("active-visite");
-			sectionKaartje.classList.toggle("active-visite");
-		}
-		else if (index == 1) {
-			console.log("tweede button");
-			buttonVisitekaartje.classList.toggle("active-Mediahuis");
-			sectionKaartje.classList.toggle("active-Mediahuis");
-		}
-		else if (index == 2) {
-            console.log("derde button");
-			buttonVisitekaartje.classList.toggle("active-Accessdash");
-			sectionKaartje.classList.toggle("active-Accessdash");
+// uncheck alle checkboxes, behalve de nieuwe die geklikt is.
+function removeChecked(clickedCheckbox) {
+	// Loop door alle checkboxes en verwijder het checked attribuut, behalve bij de nieuw geklikte checkbox.
+    checkboxes.forEach(checkbox => {
+        if (checkbox !== clickedCheckbox) {
+            checkbox.checked = false;
         }
-		else if (index == 3) {
-            console.log("vierde button");
-			buttonVisitekaartje.classList.toggle("active-ILW");
-			sectionKaartje.classList.toggle("active-ILW");
-        }
-	});
+    });
 }
 
+// luister naar een checkbox die geklikt wordt
+function newChecked() {
+    checkboxes.forEach(checkbox => {
+		// luister bij alle checkboxes naar een click
+        checkbox.addEventListener("click", (e) => {
+			// e.target is het element waarop net een interactie is uitgevoerd
+            removeChecked(e.target);
+        });
+    });
+}
 
-
-// uncheck a checkbox when a new one is checked
-document.querySelectorAll("input").forEach(checkbox => {
-	checkbox.addEventListener("change", function () {
-		document.querySelectorAll("input").forEach(other => {
-			if (other !== this) other.checked = false; // Uncheck all others
-		});
-	});
-});
+// roep de functie aan
+newChecked();
